1e87ec207fc48222f436f9fa0763502cc37b5728
Souvik Biswas <sbis1999@gmail.com>
2021-06-02 22:03:19 +0530

    Separate into different pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:dio_networking/models/user.dart';
-import 'package:dio_networking/utils/dio_client.dart';
+import 'package:dio_networking/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,55 +15,6 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  DioClient _client = DioClient();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('User Info'),
-      ),
-      body: Center(
-        child: FutureBuilder<User>(
-          future: _client.getUser(id: 1),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              User? userInfo = snapshot.data;
-
-              if (userInfo != null) {
-                Data userData = userInfo.data;
-
-                return Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Image.network(userData.avatar),
-                    SizedBox(height: 8.0),
-                    Text(
-                      '${userInfo.data.firstName} ${userInfo.data.lastName}',
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                    Text(
-                      userData.email,
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                  ],
-                );
-              }
-            }
-            return CircularProgressIndicator();
-          },
-        ),
-      ),
     );
   }
 }
