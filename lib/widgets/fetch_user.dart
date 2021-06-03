68a0fd9ae7d376367eb77f16c3938b5b4148a75f
Souvik Biswas <sbis1999@gmail.com>
2021-06-03 13:03:12 +0530

    Update fetch user widget

---
+++ lib/widgets/fetch_user.dart
@@ -0,0 +1,101 @@
+import 'package:dio_networking/models/user.dart';
+import 'package:dio_networking/utils/dio_client.dart';
+import 'package:flutter/material.dart';
+
+class FetchUser extends StatefulWidget {
+  const FetchUser({Key? key}) : super(key: key);
+
+  @override
+  _FetchUserState createState() => _FetchUserState();
+}
+
+class _FetchUserState extends State<FetchUser> {
+  final TextEditingController _idController = TextEditingController();
+  final DioClient _client = DioClient();
+
+  bool _isFetching = false;
+  bool _isDeleting = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue.shade50,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _idController,
+                decoration: InputDecoration(hintText: 'Enter ID'),
+              ),
+            ),
+            SizedBox(width: 16.0),
+            _isFetching
+                ? CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: () async {
+                      setState(() {
+                        _isFetching = true;
+                      });
+
+                      User? user = await _client.getUser(
+                        id: _idController.text,
+                      );
+
+                      if (user != null) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => Dialog(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Image.network(user.data.avatar),
+                                    Text('ID: ${user.data.id}'),
+                                    Text(
+                                      'Name: ${user.data.firstName} ${user.data.lastName}',
+                                    ),
+                                    Text('Email: ${user.data.email}'),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+
+                      setState(() {
+                        _isFetching = false;
+                      });
+                    },
+                    child: Text('Fetch'),
+                  ),
+            SizedBox(width: 16.0),
+            _isDeleting
+                ? CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: () async {
+                      setState(() {
+                        _isDeleting = true;
+                      });
+                      await _client.deleteUser(id: _idController.text);
+                      setState(() {
+                        _isDeleting = false;
+                      });
+                    },
+                    child: Text('Delete'),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
