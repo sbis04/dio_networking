f9637e9674a5f4daf730c0d08415083525e2a335
Souvik Biswas <sbis1999@gmail.com>
2021-06-03 11:20:21 +0530

    Add user info model class

---
+++ lib/models/user_info.dart
@@ -0,0 +1,22 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_info.g.dart';
+
+@JsonSerializable()
+class UserInfo {
+  String name;
+  String job;
+  String? id;
+  String? createdAt;
+
+  UserInfo({
+    required this.name,
+    required this.job,
+    this.id,
+    this.createdAt,
+  });
+
+  factory UserInfo.fromJson(Map<String, dynamic> json) =>
+      _$UserInfoFromJson(json);
+  Map<String, dynamic> toJson() => _$UserInfoToJson(this);
+}
---
+++ lib/models/user_info.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserInfo _$UserInfoFromJson(Map<String, dynamic> json) {
+  return UserInfo(
+    name: json['name'] as String,
+    job: json['job'] as String,
+    id: json['id'] as String?,
+    createdAt: json['createdAt'] as String?,
+  );
+}
+
+Map<String, dynamic> _$UserInfoToJson(UserInfo instance) => <String, dynamic>{
+      'name': instance.name,
+      'job': instance.job,
+      'id': instance.id,
+      'createdAt': instance.createdAt,
+    };
