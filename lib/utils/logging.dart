b37b702fbc3b5d6e9b6efd965ce2af4100a19da7
Souvik Biswas <sbis1999@gmail.com>
2021-06-04 07:39:46 +0530

    Add logging interceptor

---
+++ lib/utils/logging.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+
+class Logging extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    print('REQUEST[${options.method}] => PATH: ${options.path}');
+    return super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    print(
+      'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}',
+    );
+    return super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    print(
+      'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}',
+    );
+    return super.onError(err, handler);
+  }
+}
